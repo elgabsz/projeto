18851e56b7ca0654fb22f2842f82bff12d381855
elgabsz <gabriel.bellumat@iesb.edu.br>
2024-11-11 10:20:07 -0300

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Penta Calc title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Penta Calc/i);
+  expect(titleElement).toBeInTheDocument();
 });
